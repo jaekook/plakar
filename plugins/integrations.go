537e747541c5c4f7fa7a41bf0f94d1a709fc1447
Eric Faurot <eric@faurot.net>
2025-07-18 17:23:19 +0200

    update integration model and import "real" data

--- plugins/integrations.go
+++ plugins/integrations.go
@@ -9,28 +9,39 @@ import (
 	"github.com/PlakarKorp/plakar/appcontext"
 )
 
+type IntegrationInstallation struct {
+	Status  string `json:"status"`
+	Version string `json:"version,omitempty"`
+}
+
 type IntegrationTypes struct {
 	Storage     bool `json:"storage"`
 	Source      bool `json:"source"`
 	Destination bool `json:"destination"`
 }
 
-type IntegrationInstallation struct {
-	Status  string `json:"status"`
-	Version string `json:"version,omitempty"`
+type IntegrationInfo struct {
+	Id            string           `json:"id"`
+	Name          string           `json:"name"`
+	DisplayName   string           `json:"display_name"`
+	Description   string           `json:"description"`
+	Homepage      string           `json:"homepage"`
+	Repository    string           `json:"repository"`
+	License       string           `json:"license"`
+	Tags          []string         `json:"tags"`
+	APIVersion    string           `json:"api_version"`
+	LatestVersion string           `json:"latest_version"`
+	Types         IntegrationTypes `json:"types"`
+
+	Documentation string `json:"documentation"` // README.md
+	Icon          string `json:"icon"`          // assets/icon.{png,svg}
+	Featured      string `json:"featured"`      // assets/featured.{png,svg}
+
+	Installation IntegrationInstallation `json:"installation"`
 }
 
-type IntegrationInfo struct {
-	Id            string                  `json:"id"`
-	DispayName    string                  `json:"dispay_name"`
-	Description   string                  `json:"description"`
-	Documentation string                  `json:"documentation"`
-	Icon          string                  `json:"icon"`
-	Logo          string                  `json:"logo"`
-	Tags          []string                `json:"tags"`
-	LatestVersion string                  `json:"latest_version"`
-	Types         IntegrationTypes        `json:"types"`
-	Installation  IntegrationInstallation `json:"installation"`
+type IntegrationIndex struct {
+	Integrations []IntegrationInfo `json:"integrations"`
 }
 
 //go:embed integrations.json
@@ -38,14 +49,14 @@ var integrationsData []byte
 
 func IterIntegrations(ctx *appcontext.AppContext, filterType string, filterTag string) iter.Seq2[*IntegrationInfo, error] {
 	return func(yield func(*IntegrationInfo, error) bool) {
-		var list []IntegrationInfo
-		err := json.Unmarshal(integrationsData, &list)
+		var index IntegrationIndex
+		err := json.Unmarshal(integrationsData, &index)
 		if err != nil {
 			yield(nil, err)
 			return
 		}
 
-		for _, info := range list {
+		for _, info := range index.Integrations {
 			if filterType == "storage" && !info.Types.Storage {
 				continue
 			}
