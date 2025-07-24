241572d30114ba08a3bb16b7a8e2dbdb5c727b67
Gilles Chehade <gilles@poolp.org>
2025-07-25 01:53:09 +0200

    simplify the info command and usage

--- subcommands/info/errors.go
+++ subcommands/info/errors.go
@@ -1,40 +1,14 @@
 package info
 
 import (
-	"flag"
 	"fmt"
 
 	"github.com/PlakarKorp/kloset/repository"
 	"github.com/PlakarKorp/plakar/appcontext"
-	"github.com/PlakarKorp/plakar/subcommands"
 	"github.com/PlakarKorp/plakar/utils"
 )
 
-type InfoErrors struct {
-	subcommands.SubcommandBase
-
-	SnapshotID string
-}
-
-func (cmd *InfoErrors) Parse(ctx *appcontext.AppContext, args []string) error {
-	flags := flag.NewFlagSet("info errors", flag.ExitOnError)
-	flags.Parse(args)
-
-	if len(flags.Args()) < 1 {
-		return fmt.Errorf("usage: %s errors SNAPSHOT", flags.Name())
-	}
-
-	cmd.RepositorySecret = ctx.GetSecret()
-	cmd.SnapshotID = flags.Args()[0]
-
-	return nil
-}
-
-func (cmd *InfoErrors) Name() string {
-	return "info_errors"
-}
-
-func (cmd *InfoErrors) Execute(ctx *appcontext.AppContext, repo *repository.Repository) (int, error) {
+func (cmd *Info) executeErrors(ctx *appcontext.AppContext, repo *repository.Repository) (int, error) {
 	snap, pathname, err := utils.OpenSnapshotByPath(repo, cmd.SnapshotID)
 	if err != nil {
 		return 1, err
