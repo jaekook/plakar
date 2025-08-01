f9e115f412cf1814b715dc7806d2167aa91e3a8f
Gilles Chehade <gilles@poolp.org>
2025-08-02 00:01:15 +0200

    FTP moved to its own integration repository

--- connectors/ftp/fs.go
+++
@@ -1,6 +0,0 @@
-package ftp
-
-import (
-	_ "github.com/PlakarKorp/plakar/connectors/ftp/exporter"
-	_ "github.com/PlakarKorp/plakar/connectors/ftp/importer"
-)
--- main.go
+++ main.go
@@ -62,7 +62,6 @@ import (
 	_ "github.com/PlakarKorp/plakar/subcommands/version"
 
 	_ "github.com/PlakarKorp/plakar/connectors/fs"
-	_ "github.com/PlakarKorp/plakar/connectors/ftp"
 	_ "github.com/PlakarKorp/plakar/connectors/http"
 	_ "github.com/PlakarKorp/plakar/connectors/ptar"
 	_ "github.com/PlakarKorp/plakar/connectors/s3"
