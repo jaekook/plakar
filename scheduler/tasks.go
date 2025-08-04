fec3b90b54bb7533e1a6cbc1d1c04f5c9e327860
Gilles Chehade <gilles@poolp.org>
2025-08-04 16:35:23 +0200

    we no longer need to open store and repo in tasks

--- scheduler/tasks.go
+++ scheduler/tasks.go
@@ -1,15 +1,9 @@
 package scheduler
 
 import (
-	"fmt"
 	"time"
 
-	"github.com/PlakarKorp/kloset/encryption"
-	"github.com/PlakarKorp/kloset/repository"
-	"github.com/PlakarKorp/kloset/storage"
-	"github.com/PlakarKorp/kloset/versioning"
 	"github.com/PlakarKorp/plakar/agent"
-	"github.com/PlakarKorp/plakar/appcontext"
 	"github.com/PlakarKorp/plakar/locate"
 	"github.com/PlakarKorp/plakar/subcommands"
 	"github.com/PlakarKorp/plakar/subcommands/backup"
@@ -19,53 +13,6 @@ import (
 	"github.com/PlakarKorp/plakar/subcommands/rm"
 	"github.com/PlakarKorp/plakar/subcommands/sync"
 )
-
-func loadRepository(newCtx *appcontext.AppContext, name string) (*repository.Repository, storage.Store, error) {
-	if err := newCtx.ReloadConfig(); err != nil {
-		return nil, nil, fmt.Errorf("could not load configuration: %w", err)
-	}
-
-	storeConfig, err := newCtx.Config.GetRepository(name)
-	if err != nil {
-		return nil, nil, fmt.Errorf("unable to get repository configuration: %w", err)
-	}
-
-	store, config, err := storage.Open(newCtx.GetInner(), storeConfig)
-	if err != nil {
-		return nil, nil, fmt.Errorf("unable to open storage: %w", err)
-	}
-
-	repoConfig, err := storage.NewConfigurationFromWrappedBytes(config)
-	if err != nil {
-		store.Close(newCtx)
-		return nil, nil, fmt.Errorf("unable to read repository configuration: %w", err)
-	}
-
-	if repoConfig.Version != versioning.FromString(storage.VERSION) {
-		store.Close(newCtx)
-		return nil, nil, fmt.Errorf("incompatible repository version: %s != %s", repoConfig.Version, storage.VERSION)
-	}
-
-	if passphrase, ok := storeConfig["passphrase"]; ok {
-		key, err := encryption.DeriveKey(repoConfig.Encryption.KDFParams, []byte(passphrase))
-		if err != nil {
-			store.Close(newCtx)
-			return nil, nil, fmt.Errorf("error deriving key: %w", err)
-		}
-		if !encryption.VerifyCanary(repoConfig.Encryption, key) {
-			store.Close(newCtx)
-			return nil, nil, fmt.Errorf("invalid passphrase")
-		}
-		newCtx.SetSecret(key)
-	}
-
-	repo, err := repository.NewNoRebuild(newCtx.GetInner(), newCtx.GetSecret(), store, config)
-	if err != nil {
-		store.Close(newCtx)
-		return nil, store, fmt.Errorf("unable to open repository: %w", err)
-	}
-	return repo, store, nil
-}
 
 func (s *Scheduler) backupTask(taskset Task, task BackupConfig) {
 	backupSubcommand := &backup.Backup{}
@@ -96,28 +43,18 @@ func (s *Scheduler) backupTask(taskset Task, task BackupConfig) {
 				continue
 			}
 
-			repo, store, err := loadRepository(s.ctx, taskset.Repository)
-			if err != nil {
-				s.ctx.GetLogger().Error("Error loading repository: %s", err)
-				continue
-			}
-
 			if retval, err := agent.ExecuteRPC(s.ctx, []string{"backup"}, backupSubcommand, storeConfig); err != nil || retval != 0 {
 				s.ctx.GetLogger().Error("Error creating backup: %s", err)
-				goto close
+				continue
 			}
 
 			if task.Retention != 0 {
 				rmSubcommand.LocateOptions.Before = time.Now().Add(-task.Retention)
 				if retval, err := agent.ExecuteRPC(s.ctx, []string{"rm"}, rmSubcommand, storeConfig); err != nil || retval != 0 {
 					s.ctx.GetLogger().Error("Error removing obsolete backups: %s", err)
-					goto close
+					continue
 				}
 			}
-
-		close:
-			repo.Close()
-			store.Close(s.ctx)
 		}
 	}
 }
@@ -145,19 +82,10 @@ func (s *Scheduler) checkTask(taskset Task, task CheckConfig) {
 				continue
 			}
 
-			repo, store, err := loadRepository(s.ctx, taskset.Repository)
-			if err != nil {
-				s.ctx.GetLogger().Error("Error loading repository: %s", err)
-				continue
-			}
-
 			retval, err := agent.ExecuteRPC(s.ctx, []string{"check"}, checkSubcommand, storeConfig)
 			if err != nil || retval != 0 {
 				s.ctx.GetLogger().Error("Error executing check: %s", err)
 			}
-
-			repo.Close()
-			store.Close(s.ctx)
 		}
 	}
 }
@@ -184,19 +112,10 @@ func (s *Scheduler) restoreTask(taskset Task, task RestoreConfig) {
 				continue
 			}
 
-			repo, store, err := loadRepository(s.ctx, taskset.Repository)
-			if err != nil {
-				s.ctx.GetLogger().Error("Error loading repository: %s", err)
-				continue
-			}
-
 			retval, err := agent.ExecuteRPC(s.ctx, []string{"restore"}, restoreSubcommand, storeConfig)
 			if err != nil || retval != 0 {
 				s.ctx.GetLogger().Error("Error executing restore: %s", err)
 			}
-
-			repo.Close()
-			store.Close(s.ctx)
 		}
 	}
 }
@@ -236,21 +155,12 @@ func (s *Scheduler) syncTask(taskset Task, task SyncConfig) {
 				continue
 			}
 
-			repo, store, err := loadRepository(s.ctx, taskset.Repository)
-			if err != nil {
-				s.ctx.GetLogger().Error("Error loading repository: %s", err)
-				continue
-			}
-
 			retval, err := agent.ExecuteRPC(s.ctx, []string{"sync"}, syncSubcommand, storeConfig)
 			if err != nil || retval != 0 {
 				s.ctx.GetLogger().Error("sync: %s", err)
 			} else {
 				s.ctx.GetLogger().Info("sync: synchronization succeeded")
 			}
-
-			repo.Close()
-			store.Close(s.ctx)
 		}
 	}
 }
@@ -275,16 +185,10 @@ func (s *Scheduler) maintenanceTask(task MaintenanceConfig) {
 				continue
 			}
 
-			repo, store, err := loadRepository(s.ctx, task.Repository)
-			if err != nil {
-				s.ctx.GetLogger().Error("Error loading repository: %s", err)
-				continue
-			}
-
 			retval, err := agent.ExecuteRPC(s.ctx, []string{"maintenance"}, maintenanceSubcommand, storeConfig)
 			if err != nil || retval != 0 {
 				s.ctx.GetLogger().Error("Error executing maintenance: %s", err)
-				goto close
+				continue
 			} else {
 				s.ctx.GetLogger().Info("maintenance of repository %s succeeded", task.Repository)
 			}
@@ -294,15 +198,11 @@ func (s *Scheduler) maintenanceTask(task MaintenanceConfig) {
 				retval, err := agent.ExecuteRPC(s.ctx, []string{"rm"}, rmSubcommand, storeConfig)
 				if err != nil || retval != 0 {
 					s.ctx.GetLogger().Error("Error removing obsolete backups: %s", err)
-					goto close
+					continue
 				} else {
 					s.ctx.GetLogger().Info("Retention purge succeeded")
 				}
 			}
-
-		close:
-			repo.Close()
-			store.Close(s.ctx)
 		}
 	}
 }
