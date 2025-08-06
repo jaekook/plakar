a6cef1025184adc02c33fb9d8da57669a83f1c87
Julien Castets <castets.j@gmail.com>
2025-08-06 16:43:34 +0200

    plakar token: improve help
    * disallow extra arguments
    * add man page
    * fix short message

---
+++ subcommands/help/docs/plakar-token.md
@@ -0,0 +1,29 @@
+PLAKAR-TOKEN(1) - General Commands Manual
+
+# NAME
+
+**plakar-token** - Manage Plakar tokens
+
+# SYNOPSIS
+
+**plakar&nbsp;token**
+\[**create**]
+
+# DESCRIPTION
+
+The
+**plakar token**
+command manages tokens used to authenticate to Plakar services.
+Tokens are not currently usable and exist only for future features.
+
+# SUBCOMMANDS
+
+**create**
+
+> Create a new token.
+
+# SEE ALSO
+
+plakar(1)
+
+Plakar - August 6, 2025
--- subcommands/login/token-create.go
+++ subcommands/login/token-create.go
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2025 Eric Faurot <eric.faurot@plakar.io>
+ * Copyright (c) 2025 Julien Castets <julien.castets@plakar.io>
  *
  * Permission to use, copy, modify, and distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
@@ -22,32 +22,33 @@ import (
 
 	"github.com/PlakarKorp/kloset/repository"
 	"github.com/PlakarKorp/plakar/appcontext"
+	plogin "github.com/PlakarKorp/plakar/login"
 	"github.com/PlakarKorp/plakar/subcommands"
 )
 
-func init() {
-	subcommands.Register(func() subcommands.Subcommand { return &TokenCreate{} }, subcommands.AgentSupport, "token", "create")
-	subcommands.Register(func() subcommands.Subcommand { return &Token{} }, subcommands.BeforeRepositoryOpen, "token")
-}
-
-type Token struct {
+type TokenCreate struct {
 	subcommands.SubcommandBase
 }
 
-func (_ *Token) Parse(ctx *appcontext.AppContext, args []string) error {
-	flags := flag.NewFlagSet("token", flag.ExitOnError)
+func (cmd *TokenCreate) Parse(ctx *appcontext.AppContext, args []string) error {
+	flags := flag.NewFlagSet("token-create", flag.ExitOnError)
 	flags.Usage = func() {
-		fmt.Fprintf(flags.Output(), "Usage: %s create\n", flags.Name())
+		fmt.Fprintf(flags.Output(), "Usage: token create\n")
 	}
 	flags.Parse(args)
 
 	if flags.NArg() > 0 {
 		return fmt.Errorf("Too many arguments")
 	}
-
 	return nil
 }
 
-func (cmd *Token) Execute(ctx *appcontext.AppContext, repo *repository.Repository) (int, error) {
+func (cmd *TokenCreate) Execute(ctx *appcontext.AppContext, repo *repository.Repository) (int, error) {
+	token, err := plogin.DeriveToken(ctx)
+	if err != nil {
+		return 1, err
+	}
+
+	fmt.Fprintln(ctx.Stdout, token)
 	return 0, nil
 }
