ac73ff35c4603db77f6a431f2a5f9e00028be145
Julien Castets <castets.j@gmail.com>
2025-08-06 18:08:39 +0200

    Remove integrations install/uninstall in demo mode

--- api/api.go
+++ api/api.go
@@ -185,6 +185,9 @@ func SetupRoutes(server *http.ServeMux, repo *repository.Repository, ctx *appcon
 
 		server.Handle("POST /api/proxy/v1/account/notifications/set-status", authToken(JSONAPIView(ui.servicesProxy)))
 		server.Handle("PUT /api/proxy/v1/account/services/alerting", authToken(JSONAPIView(ui.servicesSetAlertingServiceConfiguration)))
+
+		server.Handle("POST /api/integrations/install", authToken(JSONAPIView(ui.integrationsInstall)))
+		server.Handle("DELETE /api/integrations/{id}", authToken(JSONAPIView(ui.integrationsUninstall)))
 	}
 
 	server.Handle("GET /api/proxy/v1/account/me", authToken(JSONAPIView(ui.servicesProxy)))
@@ -194,9 +197,6 @@ func SetupRoutes(server *http.ServeMux, repo *repository.Repository, ctx *appcon
 	server.Handle("GET /api/proxy/v1/integration", authToken(JSONAPIView(ui.servicesGetIntegration)))
 	server.Handle("GET /api/proxy/v1/integration/{id}", authToken(JSONAPIView(ui.servicesGetIntegrationId)))
 	server.Handle("GET /api/proxy/v1/integration/{id}/{path...}", authToken(JSONAPIView(ui.servicesGetIntegrationPath)))
-
-	server.Handle("POST /api/integrations/install", authToken(JSONAPIView(ui.integrationsInstall)))
-	server.Handle("DELETE /api/integrations/{id}", authToken(JSONAPIView(ui.integrationsUninstall)))
 
 	server.Handle("GET /api/repository/info", authToken(JSONAPIView(ui.repositoryInfo)))
 	server.Handle("GET /api/repository/snapshots", authToken(JSONAPIView(ui.repositorySnapshots)))
