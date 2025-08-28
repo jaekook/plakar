0ea670ff26ec736fa6085f847ea3668987d55ace
Eric Faurot <eric@faurot.net>
2025-08-28 18:16:27 +0200

    implement config file for all locate options

--- utils/config_policy.go
+++ utils/config_policy.go
@@ -6,55 +6,62 @@ import (
 	"io"
 	"os"
 	"path/filepath"
-	"slices"
 	"strconv"
+	"strings"
+	"time"
 
 	"github.com/PlakarKorp/kloset/locate"
 	"go.yaml.in/yaml/v3"
-	"gopkg.in/ini.v1"
 )
 
-type ConfigHandler interface {
-	ValidateKeyVal(key, val string) error
-	ValidateEntry(map[string]string) error
+type LocatePolicyConfig struct {
+	Before time.Time `yaml:"before,omitempty"`
+	Since  time.Time `yaml:"since,omitempty"`
+
+	Name        string `yaml:"name,omitempty"`
+	Category    string `yaml:"category,omitempty"`
+	Environment string `yaml:"environment,omitempty"`
+	Perimeter   string `yaml:"perimeter,omitempty"`
+	Job         string `yaml:"job,omitempty"`
+
+	Latest bool `yaml:"latest,omitempty"`
+
+	Tags  []string `yaml:"tags,omitempty"`
+	IDs   []string `yaml:"ids,omitempty"`
+	Roots []string `yaml:"roots,omitempty"`
+
+	Minutes    int `yaml:"minutes,omitempty"`
+	Hours      int `yaml:"hours,omitempty"`
+	Days       int `yaml:"days,omitempty"`
+	Weeks      int `yaml:"weeks,omitempty"`
+	Months     int `yaml:"months,omitempty"`
+	Years      int `yaml:"years,omitempty"`
+	Mondays    int `yaml:"mondays,omitempty"`
+	Tuesdays   int `yaml:"tuesdays,omitempty"`
+	Wednesdays int `yaml:"wednesdays,omitempty"`
+	Thursdays  int `yaml:"thursdays,omitempty"`
+	Fridays    int `yaml:"fridays,omitempty"`
+	Saturdays  int `yaml:"saturdays,omitempty"`
+	Sundays    int `yaml:"sundays,omitempty"`
+
+	PerMinute    int `yaml:"per-minute,omitempty"`
+	PerHour      int `yaml:"per-hour,omitempty"`
+	PerDay       int `yaml:"per-day,omitempty"`
+	PerWeek      int `yaml:"per-week,omitempty"`
+	PerMonth     int `yaml:"per-month,omitempty"`
+	PerYear      int `yaml:"per-year,omitempty"`
+	PerMonday    int `yaml:"per-monday,omitempty"`
+	PerTuesday   int `yaml:"per-tuesday,omitempty"`
+	PerWednesday int `yaml:"per-wednesday,omitempty"`
+	PerThursday  int `yaml:"per-thursday,omitempty"`
+	PerFriday    int `yaml:"per-friday,omitempty"`
+	PerSaturday  int `yaml:"per-saturday,omitempty"`
+	PerSunday    int `yaml:"per-sunday,omitempty"`
 }
 
 type policiesConfig struct {
-	Version  string                       `yaml:"version"`
-	Policies map[string]map[string]string `yaml:"policies"`
-}
-
-func (c *policiesConfig) ValidateKeyVal(key, value string) error {
-	if !slices.Contains([]string{
-		"keep-minutes",
-		"keep-hours",
-		"keep-days",
-		"keep-weeks",
-		"keep-months",
-		"keep-years",
-		"keep-per-minute",
-		"keep-per-hour",
-		"keep-per-day",
-		"keep-per-week",
-		"keep-per-month",
-		"keep-per-year",
-	}, key) {
-		return fmt.Errorf("invalid key")
-	}
-
-	i, err := strconv.Atoi(value)
-	if err != nil {
-		return fmt.Errorf("invalid value: %w", err)
-	}
-	if i < 0 {
-		return fmt.Errorf("negative value")
-	}
-
-	return nil
-}
-
-func (c *policiesConfig) ValidateEntry(entry map[string]string) error {
-	return nil
+	Version  string                         `yaml:"version"`
+	Policies map[string]*LocatePolicyConfig `yaml:"policies"`
 }
 
 func (c *policiesConfig) Has(name string) bool {
@@ -63,19 +70,179 @@ func (c *policiesConfig) Has(name string) bool {
 }
 
 func (c *policiesConfig) Add(name string) {
-	c.Policies[name] = make(map[string]string)
+	c.Policies[name] = &LocatePolicyConfig{}
 }
 
-func (c *policiesConfig) Set(name string, key string, value string) error {
-	if err := c.ValidateKeyVal(key, value); err != nil {
-		return err
+func (c *policiesConfig) setInt(value string, p *int) error {
+	i, err := strconv.Atoi(value)
+	if err != nil {
+		return fmt.Errorf("invalid value: %w", err)
 	}
-	c.Policies[name][key] = value
+	if i < 0 {
+		return fmt.Errorf("negative value")
+	}
+	*p = i
 	return nil
 }
 
-func (c *policiesConfig) Unset(name string, key string) {
-	delete(c.Policies[name], key)
+func (c *policiesConfig) setBool(value string, p *bool) error {
+	if value == "true" || value == "yes" {
+		*p = true
+	} else if value == "false" || value == "no" {
+		*p = false
+	} else {
+		return fmt.Errorf("invalid value")
+	}
+	return nil
+}
+
+func (c *policiesConfig) setTime(value string, p *time.Time) error {
+	t, err := locate.ParseTimeFlag(value)
+	if err != nil {
+		return fmt.Errorf("invalid value: %w", err)
+	}
+	*p = t
+	return nil
+}
+
+func (c *policiesConfig) setStringList(value string, p *[]string) error {
+	*p = strings.Split(value, ",")
+	return nil
+}
+
+func (c *policiesConfig) locateField(name string, key string) (any, error) {
+	p, ok := c.Policies[name]
+	if !ok {
+		return nil, fmt.Errorf("entry not found")
+	}
+
+	switch key {
+	case "before":
+		return &p.Before, nil
+	case "since":
+		return &p.Since, nil
+	case "name":
+		return &p.Name, nil
+	case "category":
+		return &p.Category, nil
+	case "environment":
+		return &p.Environment, nil
+	case "perimeter":
+		return &p.Perimeter, nil
+	case "job":
+		return &p.Job, nil
+	case "tags":
+		return &p.Tags, nil
+	case "ids":
+		return &p.IDs, nil
+	case "roots":
+		return &p.Roots, nil
+	case "latest":
+		return &p.Latest, nil
+	case "minutes":
+		return &p.Minutes, nil
+	case "hours":
+		return &p.Hours, nil
+	case "days":
+		return &p.Days, nil
+	case "weeks":
+		return &p.Weeks, nil
+	case "months":
+		return &p.Months, nil
+	case "years":
+		return &p.Years, nil
+	case "mondays":
+		return &p.Mondays, nil
+	case "tuesdays":
+		return &p.Tuesdays, nil
+	case "wednesdays":
+		return &p.Wednesdays, nil
+	case "thursdays":
+		return &p.Thursdays, nil
+	case "fridays":
+		return &p.Fridays, nil
+	case "saturdays":
+		return &p.Saturdays, nil
+	case "sundays":
+		return &p.Sundays, nil
+
+	case "per-minute":
+		return &p.PerMinute, nil
+	case "per-hour":
+		return &p.PerHour, nil
+	case "per-day":
+		return &p.PerDay, nil
+	case "per-week":
+		return &p.PerWeek, nil
+	case "per-month":
+		return &p.PerMonth, nil
+	case "per-year":
+		return &p.PerYear, nil
+	case "per-monday":
+		return &p.PerMonday, nil
+	case "per-tuesday":
+		return &p.PerTuesday, nil
+	case "per-wednesday":
+		return &p.PerWednesday, nil
+	case "per-thursday":
+		return &p.PerThursday, nil
+	case "per-friday":
+		return &p.PerFriday, nil
+	case "per-saturday":
+		return &p.PerSaturday, nil
+	case "per-sunday":
+		return &p.PerSunday, nil
+
+	default:
+		return nil, fmt.Errorf("invalid key")
+	}
+}
+
+func (c *policiesConfig) Set(name string, key string, value string) error {
+	field, err := c.locateField(name, key)
+	if err != nil {
+		return err
+	}
+
+	switch p := field.(type) {
+	case *int:
+		return c.setInt(value, p)
+	case *time.Time:
+		return c.setTime(value, p)
+	case *string:
+		*p = value
+		return nil
+	case *[]string:
+		return c.setStringList(value, p)
+	case *bool:
+		return c.setBool(value, p)
+	default:
+		return fmt.Errorf("invalid field type")
+	}
+}
+
+func (c *policiesConfig) Unset(name string, key string) error {
+	field, err := c.locateField(name, key)
+	if err != nil {
+		return err
+	}
+
+	switch p := field.(type) {
+	case *int:
+		*p = 0
+	case *time.Time:
+		*p = time.Time{}
+	case *string:
+		*p = ""
+	case *[]string:
+		*p = nil
+	case *bool:
+		*p = false
+	default:
+		return fmt.Errorf("invalid field type")
+	}
+
+	return nil
 }
 
 func (c *policiesConfig) Remove(name string) {
@@ -100,17 +267,6 @@ func (c *policiesConfig) Load(rd io.Reader) error {
 	return yaml.NewDecoder(rd).Decode(c)
 }
 
-func marshalINISections(sectionName string, kv map[string]string, w io.Writer) error {
-	cfg := ini.Empty()
-
-	section := cfg.Section(sectionName)
-	for key, value := range kv {
-		section.Key(key).SetValue(value)
-	}
-	_, err := cfg.WriteTo(w)
-	return err
-}
-
 func (c *policiesConfig) Dump(w io.Writer, format string, names []string) error {
 
 	if len(names) == 0 {
@@ -126,11 +282,9 @@ func (c *policiesConfig) Dump(w io.Writer, format string, names []string) error 
 		var err error
 		switch format {
 		case "json":
-			err = json.NewEncoder(w).Encode(map[string]map[string]string{name: c.Policies[name]})
-		case "ini":
-			err = marshalINISections(name, c.Policies[name], w)
+			err = json.NewEncoder(w).Encode(map[string]*LocatePolicyConfig{name: c.Policies[name]})
 		case "yaml":
-			err = yaml.NewEncoder(w).Encode(map[string]map[string]string{name: c.Policies[name]})
+			err = yaml.NewEncoder(w).Encode(map[string]*LocatePolicyConfig{name: c.Policies[name]})
 		default:
 			return fmt.Errorf("unknown format %q", format)
 		}
@@ -142,39 +296,10 @@ func (c *policiesConfig) Dump(w io.Writer, format string, names []string) error 
 	return nil
 }
 
-func (c *policiesConfig) ApplyConfig(name string, po *locate.LocateOptions) {
-	apply := func(setter func(int) locate.Option, key string) {
-		entry, ok := c.Policies[name]
-		if !ok {
-			return
-		}
-		value, ok := entry[key]
-		if !ok {
-			return
-		}
-		i, err := strconv.Atoi(value)
-		if err != nil {
-			return
-		}
-		setter(i)(po)
-	}
-	apply(locate.WithKeepMinutes, "keep-minutes")
-	apply(locate.WithKeepHours, "keep-hours")
-	apply(locate.WithKeepDays, "keep-days")
-	apply(locate.WithKeepWeeks, "keep-weeks")
-	apply(locate.WithKeepMonths, "keep-months")
-	apply(locate.WithKeepYears, "keep-years")
-	apply(locate.WithPerMinuteCap, "keep-per-minute")
-	apply(locate.WithPerHourCap, "keep-per-hour")
-	apply(locate.WithPerDayCap, "keep-per-day")
-	apply(locate.WithPerWeekCap, "keep-per-week")
-	apply(locate.WithPerMonthCap, "keep-per-month")
-	apply(locate.WithPerYearCap, "keep-per-year")
-}
-
 func LoadPolicyConfigFile(filename string) (*policiesConfig, error) {
 	var cfg policiesConfig
-	cfg.Policies = make(map[string]map[string]string)
+	cfg.Version = "v1.0.0"
+	cfg.Policies = make(map[string]*LocatePolicyConfig)
 
 	rd, err := os.Open(filename)
 	if err != nil {
@@ -190,4 +315,54 @@ func LoadPolicyConfigFile(filename string) (*policiesConfig, error) {
 	}
 
 	return &cfg, nil
+}
+
+func (c *policiesConfig) ApplyConfig(name string, po *locate.LocateOptions) {
+	p, ok := c.Policies[name]
+	if !ok {
+		return
+	}
+
+	po.Filters.Before = p.Before
+	po.Filters.Since = p.Since
+
+	po.Filters.Name = p.Name
+	po.Filters.Category = p.Category
+	po.Filters.Environment = p.Environment
+	po.Filters.Perimeter = p.Perimeter
+	po.Filters.Job = p.Job
+
+	po.Filters.Latest = p.Latest
+
+	po.Filters.Tags = p.Tags
+	po.Filters.IDs = p.IDs
+	po.Filters.Roots = p.Roots
+
+	po.Minute.Keep = p.Minutes
+	po.Hour.Keep = p.Hours
+	po.Day.Keep = p.Days
+	po.Week.Keep = p.Weeks
+	po.Month.Keep = p.Months
+	po.Year.Keep = p.Years
+	po.Monday.Keep = p.Mondays
+	po.Tuesday.Keep = p.Tuesdays
+	po.Wednesday.Keep = p.Wednesdays
+	po.Thursday.Keep = p.Thursdays
+	po.Friday.Keep = p.Fridays
+	po.Saturday.Keep = p.Saturdays
+	po.Sunday.Keep = p.Sundays
+
+	po.Minute.Cap = p.PerMinute
+	po.Hour.Cap = p.PerHour
+	po.Day.Cap = p.PerDay
+	po.Week.Cap = p.PerWeek
+	po.Month.Cap = p.PerMonth
+	po.Year.Cap = p.PerYear
+	po.Monday.Cap = p.PerMonday
+	po.Tuesday.Cap = p.PerTuesday
+	po.Wednesday.Cap = p.PerWednesday
+	po.Thursday.Cap = p.PerThursday
+	po.Friday.Cap = p.PerFriday
+	po.Saturday.Cap = p.PerSaturday
+	po.Sunday.Cap = p.PerSunday
 }
