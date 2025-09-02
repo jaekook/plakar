ffac1538137506877d7b574a4637bbe0ea17070c
Eric Faurot <eric@faurot.net>
2025-09-02 15:39:00 +0200

    improve usage output

--- subcommands/agent/agent_stop.go
+++ subcommands/agent/agent_stop.go
@@ -34,8 +34,7 @@ type AgentStop struct {
 func (cmd *AgentStop) Parse(ctx *appcontext.AppContext, args []string) error {
 	flags := flag.NewFlagSet("agent stop", flag.ExitOnError)
 	flags.Usage = func() {
-		fmt.Fprintf(flags.Output(), "Usage: %s [OPTIONS]\n", flags.Name())
-		fmt.Fprintf(flags.Output(), "\nOPTIONS:\n")
+		fmt.Fprintf(flags.Output(), "Usage: %s\n", flags.Name())
 		flags.PrintDefaults()
 	}
 	flags.Parse(args)
